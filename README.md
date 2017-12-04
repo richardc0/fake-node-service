84379263d8280da4e86c0ad4c9178a05f0880205
Richard Chapman <richard.chapman@digital.cabinet-office.gov.uk>
2017-12-04 13:00:19 +0000

    Code for sample service to run on PaaS with CF push.

---
+++ README.md
@@ -0,0 +1,9 @@
+fake-node-service
+
+Created to fake a real service for performance bench marking and testing a call back.
+
+DOES NOT process the data just pauses and returns a valid response (200).
+
+To deploy on PaaS cf push <paas-app-name>
+
+
---
+++ example.js
@@ -0,0 +1,50 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api v2!' });   
+});
+
+// more routes for our API will happen here
+
+router.route('/test')
+
+    .get(function(req, res) {
+      res.json({ message: 'Success' });
+    })
+
+    // create a bear (accessed at POST http://localhost:8080/api/test)
+    .post(function(req, res) {
+
+      var sleep = require('system-sleep');
+      sleep(50);
+      res.json({ message: 'Success' });
+    });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
